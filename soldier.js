7e3b3ecef4e0d50985ee20670269c020ec4b29cb
ikotiev <ikotiev@mail.ru>
2022-11-01 21:37:30 +0300

    закончил 1 таск 3го дня 2ой недели Объекты: солдат

--- soldier.js
+++ soldier.js
@@ -1,17 +1,43 @@
 const soldier = {
     name: 'Халк',
     health: 10,
-    weapon {
+    weapon: {
       model: 'ПП',
       bullets: 30,
     },
     supplies: 3,
   
     shoot() {
+        let text;
+        if(this.weapon.bullets >= 1){
+            this.weapon.bullets -= 1; 
+            text = 'Бах-бах';
+        }else if(this.weapon.bullets < 1){
+            text = 'Обойма пуста. Перезарядитесь';
+        } 
+        return text
 
     },
     recharge() {
-
+        let text;
+        if(this.weapon.supplies >= 1){
+            this.weapon.supplies -=1;
+            this.weapon.bullet ==30;
+            text = 'Перезарядка...'; 
+        }else{
+            text = 'Не осталось припасов';
+        }
+        return text;
     },
-    hurt() {},
-  };
+    hurt() {
+        let text = 'Ты проиграл!';
+        if(this.health >= 1){
+            this.health -= 1;
+        }else{
+            text = 'Ты проиграл!';
+
+        }
+        return text;
+    }
+} 
+/*console.log(soldier.shoot(), soldier.recharge(), soldier.hurt());*/
