f9cf0607b1f50c80e6ec50506f3e663af0d41373
ikotiev <ikotiev@mail.ru>
2022-11-01 19:48:22 +0300

    написал начальный код

---
+++ soldier.js
@@ -0,0 +1,17 @@
+const soldier = {
+    name: 'Халк',
+    health: 10,
+    weapon {
+      model: 'ПП',
+      bullets: 30,
+    },
+    supplies: 3,
+  
+    shoot() {
+
+    },
+    recharge() {
+
+    },
+    hurt() {},
+  };
